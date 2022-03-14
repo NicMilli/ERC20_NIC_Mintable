592ac4e70cbc4fc2b5d78d18832a1713905e0ec5
NicMilli <91165462+NicMilli@users.noreply.github.com>
2022-03-14 16:38:08 -0400

    Updates

---
+++ precache-manifest.a47f11f7d940f1d68052eb52e13c8bae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c9c6c21880b22278938779e3a5acc6",
+    "url": "/ERC20_NIC_Mintable/index.html"
+  },
+  {
+    "revision": "a108996bd62e3ca17ee8",
+    "url": "/ERC20_NIC_Mintable/static/css/main.4873a20c.chunk.css"
+  },
+  {
+    "revision": "1e11b4d05b67c731abd4",
+    "url": "/ERC20_NIC_Mintable/static/js/2.f0e30f20.chunk.js"
+  },
+  {
+    "revision": "a108996bd62e3ca17ee8",
+    "url": "/ERC20_NIC_Mintable/static/js/main.6d9b905a.chunk.js"
+  },
+  {
+    "revision": "7a5ff32b0be8d6797fd0",
+    "url": "/ERC20_NIC_Mintable/static/js/runtime-main.e5c37a41.js"
+  },
+  {
+    "revision": "7937050ebe52c3567205778e3d1fd14c",
+    "url": "/ERC20_NIC_Mintable/static/media/bckgrnd.7937050e.jpg"
+  },
+  {
+    "revision": "95b727084ce9c269fa56d19f6a1b5358",
+    "url": "/ERC20_NIC_Mintable/static/media/extension1.95b72708.png"
+  },
+  {
+    "revision": "4f31e465ddc6a31d8c38fe9efa8fb662",
+    "url": "/ERC20_NIC_Mintable/static/media/extension2.4f31e465.png"
+  },
+  {
+    "revision": "b8d3a56101966b2394dfad32bbb3d5df",
+    "url": "/ERC20_NIC_Mintable/static/media/mylogo.b8d3a561.png"
+  }
+]);
